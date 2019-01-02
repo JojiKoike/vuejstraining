00d8c475b184cd0ea4397747073875ad78fde2f9
JojiKoike <joji.koike@gmail.com>
2019-01-02 23:58:12 +0900

    Object Data Binding Sample

--- chapter2/js/main.js
+++ chapter2/js/main.js
@@ -1,6 +1,14 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Vue.js!'
+    message: {
+      value: 'Hello, Vue.js!',
+      scroll: 0
+    },
+    list: ['綾瀬はるか', '土屋太鳳', '深田恭子'],
+    num: 1
+  },
+  mounted: function(){
+    this.scroll = 100;
   }
 });
